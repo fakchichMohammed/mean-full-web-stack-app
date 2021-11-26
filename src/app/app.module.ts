2d017c5d02e6e32772f397438fd6326c1ea7e5ae
mohammed fakchich <mohammed.fakchich@gmail.com>
2021-11-26 19:13:32 +0100

    added: pagination with angular & express

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatCardModule} from '@angular/material/card'
 import { MatButtonModule} from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './pages/posts/post-create/post-create.component';
@@ -37,7 +38,8 @@ import { PostListComponent } from './pages/posts/post-list/post-list.component';
     MatToolbarModule,
     MatExpansionModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/posts/post-list/post-list.component.ts
+++ src/app/pages/posts/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
 import { PostsService } from 'src/app/apis/services/posts.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-post-list',
@@ -13,16 +14,21 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   private postsSub: Subscription;
   isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 5;
+  currentPage = 1;
+  pageSizeOptions = [1, 10, 25, 100];
 
   constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListener()
-      .subscribe((posts: Post[]) => {
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
         this.isLoading = false;
-        this.posts = posts;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
       });
   }
 
@@ -30,8 +36,18 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.postsSub.unsubscribe();
   }
 
-  onDelete(postId: string) {    
-    this.postsService.deletePost(postId);
+  onDelete(postId: string) { 
+    this.isLoading =  true;   
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    });
+  }
+
+  onChangedPage(pageEvent: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageEvent.pageIndex + 1;
+    this.postsPerPage = pageEvent.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage)
   }
 
 }

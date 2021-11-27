d0a050f52e0838903ea2fcac424f8f2b89e9caf4
mohammed fakchich <mohammed.fakchich@gmail.com>
2021-11-27 20:55:54 +0100

    added: route-guards

--- src/app/pages/posts/post-list/post-list.component.ts
+++ src/app/pages/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
 import { PostsService } from 'src/app/apis/services/posts/posts.service';
 import { PageEvent } from '@angular/material/paginator';
+import { AuthService } from 'src/app/apis/services/auth/auth.service';
 
 @Component({
   selector: 'app-post-list',
@@ -10,30 +11,40 @@ import { PageEvent } from '@angular/material/paginator';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
+  
+  private postsSub: Subscription;
+  private authStatusSubs: Subscription;
 
   posts: Post[] = [];
-  private postsSub: Subscription;
   isLoading = false;
   totalPosts = 0;
   postsPerPage = 5;
   currentPage = 1;
   pageSizeOptions = [1, 10, 25, 100];
+  userIsAuthenticated = false;
 
-  constructor(public postsService: PostsService) { }
+  constructor(public postsService: PostsService, private auth: AuthService) {}
 
   ngOnInit(): void {
     this.isLoading = true;
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
-    this.postsSub = this.postsService.getPostUpdateListener()
-      .subscribe((postData: {posts: Post[], postCount: number}) => {
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((postData: {
+      posts: Post[], 
+      postCount: number
+    }) => {
         this.isLoading = false;
         this.totalPosts = postData.postCount;
         this.posts = postData.posts;
-      });
+    });
+    this.userIsAuthenticated = this.auth.getIsAuth();
+    this.authStatusSubs = this.auth.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    })
   }
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.authStatusSubs.unsubscribe();
   }
 
   onDelete(postId: string) { 

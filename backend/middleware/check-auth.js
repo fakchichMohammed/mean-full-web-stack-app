3b239899b1c39cac4bd9a8a27d9124f3031343ed
mohammed fakchich <mohammed.fakchich@gmail.com>
2021-11-28 20:24:50 +0100

    added: user-model relation

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "secret_this-should_be_longer_more_longer_more_secure_will_be_our_token_just_like_this");
+    const decodedToken = jwt.verify(token, "secret_this-should_be_longer_more_longer_more_secure_will_be_our_token_just_like_this");
+    req.userData = {
+      email: decodedToken.email,
+      userId: decodedToken.userId
+    }
     next();  
   } catch (error) {
     res.status(401).json({
--- backend/models/post.js
+++ backend/models/post.js
@@ -9,7 +9,15 @@ const postSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  imagePath: { type: String, required: true },
+  imagePath: { 
+    type: String, 
+    required: true 
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", 
+    require: true
+  }
 });
 
 module.exports = mongoose.model("Post", postSchema);
